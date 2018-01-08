8ac1e36034471f8443df4ac69ae2c26217fcd762
chinann6213 <chinann6213@gmail.com>
2018-01-08 23:23:36 +0800

    modified index.php

--- index.php
+++ index.php
@@ -2,16 +2,13 @@
   include_once 'header.php';
 ?>
 
-<html>
-  <head>
-    <!-- CSS -->
-  </head>
-  <body>
-    <!-- JS -->
-    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/index.js"></script>
-  </body>
-</html>
+<main>
+  <p>test</p>
+  <!-- JS -->
+  <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+  <script type="text/javascript" src="js/index.js"></script>
+</main>
+
 
 <?php
   include_once 'footer.php';
