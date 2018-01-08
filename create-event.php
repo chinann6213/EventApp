2eebc2815ccdcd654cf97e9ab83d7f352a50a4fd
chinann6213 <chinann6213@gmail.com>
2018-01-09 00:23:42 +0800

    added event functionalities and open icons

---
+++ create-event.php
@@ -0,0 +1,11 @@
+<?php include_once "sidebar.php" ?>
+
+<link rel="stylesheet" href="css/create-event.css">
+<div id="create-event">
+  <div id="ce-header"><p>Create Event</p></div>
+  <div id="ce-container"></div>
+</div>
+
+<script type="text/javascript" src="js/sidebar.js"></script>
+</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,23 @@
+<?php
+    // check user login code here
+?>
+
+<html>
+  <head>
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/sidebar.css">
+  </head>
+  <body>
+    <div id="nav-sidebar">
+      <div id="user-login-details" class="main-item">Hi, Batman</div>
+      <div class="main-item" id="manage-event">
+        <p><img class="icon" src="open-iconic/svg/calendar.svg"><span>Event Manager</span></p>
+        <div class="manage-event">
+          <div class="sub-item">Create Event</div>
+          <div class="sub-item">Manage Event</div>
+          <div class="sub-item">Categories</div>
+        </div>
+      </div>
+      <div class="main-item" id="gallery"><p><img class="icon" src="open-iconic/svg/image.svg"><span>Gallery</span></p></div>
+      <div class="main-item" id="about-us"><p><img class="icon" src="open-iconic/svg/info.svg"><span>About Us</span></p></div>
+    </div>
