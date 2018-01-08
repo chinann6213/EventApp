89675d74696cd39ca605c4f7e0dc7766a1e3415d
chinann6213 <chinann6213@gmail.com>
2018-01-08 23:29:41 +0800

    add jquery to header

--- header.php
+++ header.php
@@ -20,3 +20,4 @@
       </ul>
     </nav>
   </header>
+  <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <main>
   <p>test</p>
   <!-- JS -->
-  <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
   <script type="text/javascript" src="js/index.js"></script>
 </main>
 
