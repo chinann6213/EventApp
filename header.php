64d6d8e74c4ffa85d38f9ac1176b13f5c9f8893e
theflybirdstudio <ken_gaming@hotmail.com>
2018-01-08 23:22:06 +0800

    Added main page content

--- header.php
+++ header.php
@@ -3,18 +3,18 @@
 <head>
   <meta charset="utf-8">
   <title>Event App</title>
-  <link rel="stylesheet" href="css/styles.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css?family=Catamaran|Cormorant+Garamond" rel="stylesheet">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
   <header>
     <a href="index.html" class="header-brand">EventApp</a>
     <nav>
       <ul>
-        <li><a href="">Browse Event</a></li>
         <li><a href="">Create Event</a></li> <!-- if user logged in -->
-        <li><a href="">My Event</a><li> <!-- if use logged in -->
+        <li><a href="">My Event</a></li> <!-- if use logged in -->
         <li><a href="">Sign In</a></li> <!-- if user hasn't login -->
         <li><a href="">Sign Up</a></li> <!-- if user hasn't login -->
       </ul>
--- index.php
+++ index.php
@@ -3,6 +3,23 @@
 ?>
 
 <main>
+  <section class="index-banner">
+    <div class="vertical-center">
+      <h1>Welcome to EventApp</h1>
+      <h2>Find your next event here !</h2>
+      <div class="search-container">
+        <form action="" method="post">
+          <input type="text" placeholder="Search for next event.." name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+      </div>
+    </div>
+  </section>
+  <div class="wrapper">
+    <section class="index-event-list">
+      <h1>Events for you</h1>
+    </section>
+  </div>
 </main>
 
 <?php
