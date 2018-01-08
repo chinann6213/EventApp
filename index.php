24ef345101117ce98bc15dcb24e387f22b66f6f1
chinann6213 <chinann6213@gmail.com>
2018-01-08 23:12:12 +0800

    created js folder and added links

--- index.php
+++ index.php
@@ -2,8 +2,16 @@
   include_once 'header.php';
 ?>
 
-<main>
-</main>
+<html>
+  <head>
+    <!-- CSS -->
+  </head>
+  <body>
+    <!-- JS -->
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
+  </body>
+</html>
 
 <?php
   include_once 'footer.php';
