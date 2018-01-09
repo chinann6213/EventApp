31b9e5a6cbc4029e23398898e957e462b1799784
theflybirdstudio <ken_gaming@hotmail.com>
2018-01-09 21:59:49 +0800

    Added dummy html on main page

--- index.php
+++ index.php
@@ -9,15 +9,71 @@
       <h2>Find your next event here !</h2>
       <div class="search-container">
         <form action="" method="post">
-          <input type="text" placeholder="Search for next event.." name="search">
+          <input type="text" placeholder="Search for next event.." name="search"/>
           <button type="submit"><i class="fa fa-search"></i></button>
         </form>
       </div>
     </div>
   </section>
-  <div class="wrapper">
+  <div class="container">
     <section class="index-event-list">
       <h1>Events for you</h1>
+      <div class="row">
+        <div class="column">
+          <div id="event-card">
+            <h1> Lelong.my: Class 101 - Now Everybody Can Sell Online! (English)</h1>
+            <h2> Now Everybody Can Sell Online! (English)... </h2>
+            <h3> (Kuala Lumpur, 6th Jan) </h3>
+            <button type="submit" value="Register">Register</button>
+          </div>
+        </div>
+        <div class="column">
+          <div id="event-card">
+            <h1> Lelong.my: Class 101 - Now Everybody Can Sell Online! (English)</h1>
+            <h2> Now Everybody Can Sell Online! (English)... </h2>
+            <h3> (Kuala Lumpur, 6th Jan) </h3>
+            <button type="submit" value="Register">Register</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="column">
+          <div id="event-card">
+            <h1> Lelong.my: Class 101 - Now Everybody Can Sell Online! (English)</h1>
+            <h2> Now Everybody Can Sell Online! (English)... </h2>
+            <h3> (Kuala Lumpur, 6th Jan) </h3>
+            <button type="submit" value="Register">Register</button>
+          </div>
+        </div>
+        <div class="column">
+          <div id="event-card">
+            <h1> Lelong.my: Class 101 - Now Everybody Can Sell Online! (English)</h1>
+            <h2> Now Everybody Can Sell Online! (English)...  </h2>
+            <h3> (Kuala Lumpur, 6th Jan) </h3>
+            <button type="submit" value="Register">Register</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="column">
+          <div id="event-card">
+            <h1> Lelong.my: Class 101 - Now Everybody Can Sell Online! (English)</h1>
+            <h2> Now Everybody Can Sell Online! (English)... </h2>
+            <h3> (Kuala Lumpur, 6th Jan) </h3>
+            <button type="submit" value="Register">Register</button>
+          </div>
+        </div>
+        <div class="column">
+          <div id="event-card">
+            <h1> Lelong.my: Class 101 - Now Everybody Can Sell Online! (English)</h1>
+            <h2> Now Everybody Can Sell Online! (English)...  </h2>
+            <h3> (Kuala Lumpur, 6th Jan) </h3>
+            <button type="submit" value="Register">Register</button>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </main>
