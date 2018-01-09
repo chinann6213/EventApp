086f8ed458d78061a8546a1fb2c79e0ffc311f64
theflybirdstudio <ken_gaming@hotmail.com>
2018-01-09 16:09:37 +0800

    added main page content

--- index.php
+++ index.php
@@ -3,11 +3,24 @@
 ?>
 
 <main>
-  <p>test</p>
-  <!-- JS -->
-  <script type="text/javascript" src="js/index.js"></script>
+  <section class="index-banner">
+    <div class="vertical-center">
+      <h1>Welcome to EventApp</h1>
+      <h2>Find your next event here !</h2>
+      <div class="search-container">
+        <form action="" method="post">
+          <input type="text" placeholder="Search for next event.." name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+      </div>
+    </div>
+  </section>
+  <div class="wrapper">
+    <section class="index-event-list">
+      <h1>Events for you</h1>
+    </section>
+  </div>
 </main>
-
 
 <?php
   include_once 'footer.php';
