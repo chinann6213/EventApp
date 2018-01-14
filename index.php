e42ae08cc105886a6ab235db4de70a8e7b7370c1
theflybirdstudio <ken_gaming@hotmail.com>
2018-01-14 20:33:16 +0800

    add

--- index.php
+++ index.php
@@ -5,18 +5,9 @@
 ?>
 
 <main>
-  <section class="index-banner">
-    <div class="vertical-center">
-      <h1>Welcome to EventApp</h1>
-      <h2>Find your next event here !</h2>
-      <div class="search-container">
-        <form action="" method="post">
-          <input type="text" placeholder="Search for next event.." name="search"/>
-          <button type="submit"><i class="fa fa-search"></i></button>
-        </form>
-      </div>
-    </div>
-  </section>
+  <?php
+    include_once 'index-banner.php'
+  ?>
   <div class="container">
     <section class="index-event-list">
       <h1>Events for you</h1>
