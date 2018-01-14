f4551fe57af52592d54a5fb4a9033a7facd23a8c
ganqitze <ganqitze@gmail.com>
2018-01-14 21:07:00 +0800

    registration pop up form

--- view-event.php
+++ view-event.php
@@ -5,6 +5,7 @@ include_once 'header.php';
 <main>
   <!-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> -->
   <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+
   <div class="container">
     <section class="">
       <div class="row">
@@ -53,8 +54,28 @@ include_once 'header.php';
 
           <!-- Check if tickets left -->
           <?php
-          if (false) {
-            echo '<button type="submit" value="Register">Register</button>';
+          if (true) {
+            echo '<button id="regBtn" type="submit" value="Register">Register</button>';
+
+            echo '<div id="regModal" class="modal">';
+            echo '<div class="modal-content">';
+            echo '<span class="close">&times;</span>';
+            echo '<h3>Register information</h3>';
+            echo '<br>';
+            echo '<form method="post" action="#">';
+            echo '<div class="info-form">';
+            echo '<input type="hidden" name="eventID" value="eventID"/>';
+            echo '<label>Name: <input type="text" name="name" autofocus required/></label>';
+            echo '<br><br>';
+            echo '<label>E-mail Address: <input type="text" name="email" required/></label>';
+            echo '<br><br>';
+            echo '<label>Contact Number: <input type="tel" name="phone" pattern="^\d{3}-\d{7}$" required/></label>';
+            echo '<br><br>';
+            echo '<input type="submit" value="Submit" id="reg-btn">';
+            echo '</div>';
+            echo '</form>';
+            echo '</div>';
+            echo '</div>';
           } 
           else {
             echo '<div id="no-ticket">Sold Out!</div>';
@@ -65,8 +86,65 @@ include_once 'header.php';
 
     </section>
   </div>
+
+
+
+
 </main>
 <style type="text/css">
+
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 30%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button */
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.info-form label {
+  padding: 10px 0px;
+}
+
+#reg-btn {
+  font-size: 100%;
+  font-family: Catamaran;
+  margin: 0 auto;
+  background-color: #5C6BC0;
+  color: #fff;
+  font-weight: 400;
+  text-decoration: none;
+  display: block;
+  border: 1.5px solid #283593;
+}
 
 #event-view {
   padding: 20px 20px;
@@ -82,6 +160,14 @@ include_once 'header.php';
   color: #111;
   text-transform: uppercase;
   padding: 30px 0px 10px 0px;
+}
+
+#event-view h3 {
+  font-family: Catamaran;
+  font-size: 18px;
+  color: #111;
+  text-transform: uppercase;
+  padding: 10px 0px 10px 0px;
 }
 
 #event-view button {
@@ -125,7 +211,6 @@ include_once 'header.php';
 }
 
 #event-img img {
-  /*width: 50%;*/
   max-width: 960px;
   min-width: 500px;
   margin: 30px auto 15px auto; 
@@ -133,6 +218,34 @@ include_once 'header.php';
 }
 
 </style>
+
+<script type="text/javascript">
+// Get the modal
+var modal = document.getElementById('regModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("regBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
 
 
 <?php
