9792bbd63b8fa96c21261abd9eda021b927eda4a
chinann6213 <chinann6213@gmail.com>
2018-01-16 15:12:05 +0800

    remove errors in save event

--- manage-event.php
+++ manage-event.php
@@ -7,23 +7,27 @@
     <p>Manage Event</p>
   </div>
   <div id="me-container">
-      <table>
+      <table cellspacing="0">
         <thead>
-            <th id="check-table"><label><input type="checkbox" />Select All</label></th>
-            <th id="event-detail">Events</th>
-            <th id="event-location">Location</th>
-            <th id="event-date">Date Created</th>
-            <th id="event-action">Action</th>
+            <tr>
+                <th id="check-table"><label><input type="checkbox" /><span>Select All</span></label></th>
+                <th id="event-detail">Events</th>
+                <th id="event-location">Location</th>
+                <th id="event-date">Date Created</th>
+                <th id="event-action">Action</th>
+            </tr>
         </thead>
         <tbody>
-            <td><label><input type="checkbox" /></label></td>
-            <td>
-                <h6>Title</h6>
-                <p>Content 123123123 Content 123123123 Content 123123123 Content 123123123 Content 123123123 Content 123123123 Content 123123123</p>
-            </td>
-            <td>Kuala Lumpur</td>
-            <td>20 January 2018, 20:02</td>
-            <td><button>Edit</button><span> | </span><button>Delete</button></td>
+            <tr>
+                <td class="select"><label><input type="checkbox" /></label></td>
+                <td class="event-detail">
+                    <h6>Title</h6>
+                    <p>Content 123123123 Content 123123123 Content 123123123 Content 123123123 Content 123123123 Content 123123123 Content 123123123</p>
+                </td>
+                <td>Kuala Lumpur</td>
+                <td>20 January 2018, 20:02</td>
+                <td><button>Edit</button><span> | </span><button>Delete</button></td>
+            </tr>
         </tbody>
       </table>
   </div>
