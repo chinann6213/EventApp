c51fee1e78d8efd3695b1dd3b0d1a1323f6c5654
ganqitze <ganqitze@gmail.com>
2018-01-19 12:52:32 +0800

    update php

--- ajax/load_event.php
+++ ajax/load_event.php
@@ -13,34 +13,40 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$event_id = $_GET['event-id'];
-$sql = "SELECT * FROM event WHERE event_id = '$event_id'";
-$results = mysqli_query($conn, $sql);
-while($event_data = mysqli_fetch_assoc($results)) {
-    $event_title = $event_data['event_title'];
-    $event_content = $event_data['event_content'];
-    $event_start_date = $event_data['event_start_date'];
-    $event_start_time = $event_data['event_start_time'];
-    $event_end_date = $event_data['event_end_date'];
-    $event_longitude = $event_data['event_longitude'];
-    $event_latitude = $event_data['event_latitude'];
-}
+if (isset($_GET['event_id'])){
+    $event_id = $_GET['event_id'];
+    $sql = "SELECT * FROM event WHERE event_id = '$event_id'";
+    $results = mysqli_query($conn, $sql);
+    while($event_data = mysqli_fetch_assoc($results)) {
+        $event_title = $event_data['event_title'];
+        $event_content = $event_data['event_content'];
+        $event_start_date = $event_data['event_start_date'];
+        $event_start_time = $event_data['event_start_time'];
+        $event_end_date = $event_data['event_end_date'];
+        $event_longitude = $event_data['event_longitude'];
+        $event_latitude = $event_data['event_latitude'];
+    }
+
+} 
 
 
 ?>
 
 <html>
 <head>
-    <link rel="stylesheet" href="css/view-event.css">
+    <link rel="stylesheet" type="text/css" href="css/view-event.css">
     <script type="text/javascript" src="js/view-event.js"></script>
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWAtE05RIq6Wd1xmHsLd2BXbC2fd0xhs&callback=initMap"></script>
 </head>
 <body>
     <div class="container">
         <div id="event-view">
-            <div id="event-name">
+            <div>
                 <h1><?php echo $event_title; ?></h1>
-                <?php echo $event_start_date.' at '.$event_start_time.' - '.$event_end_date; ?>
+                <span id="event-date">
+                    <?php echo $event_start_date.' at '.$event_start_time.' - '.$event_end_date; ?>
+                </span>
                 <!-- Monday, 29 October 2018 at 09:00 - Friday, 2 November 2018 at 17:00 -->
                 <!-- <br><i>tag (if any)</i> -->
             </div>
