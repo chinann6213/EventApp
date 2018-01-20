dabafe836bd83121ec1456fe61240d17a9cbe668
chinann6213 <chinann6213@gmail.com>
2018-01-21 04:15:34 +0800

    add unlink images for deleted event

--- ajax/delete_event.php
+++ ajax/delete_event.php
@@ -16,6 +16,15 @@ if (!$conn) {
 $eid = $_POST['event_id'];
 $sql = "DELETE FROM event WHERE event_id = '$eid' AND event_author = '$user'";
 if (mysqli_query($conn, $sql)) {
+    $img = array();
+    $sql = "SELECT img_src FROM gallery WHERE event_id = '$eid' AND img_owner = '$user'";
+    $result = mysqli_query($conn, $sql);
+    while ($del = mysqli_fetch_assoc($result)) {
+        $img[] = "../".$del['img_src'];
+    }
+    for ($i = 0; $i < count($img); $i ++) {
+        unlink($img[$i]);
+    }
     $sql = "DELETE FROM gallery WHERE event_id = '$eid' AND img_owner = '$user'";
     if (mysqli_query($conn, $sql)) {
         echo "SUCCESS";
--- sql/mysqli_connect.php
+++ sql/mysqli_connect.php
@@ -1,7 +1,7 @@
 <?php
 
   DEFINE ('DB_USER', 'root');
-  DEFINE ('DB_PASSWORD', '971127');
+  DEFINE ('DB_PASSWORD', '');
   DEFINE ('DB_HOST', 'localhost');
   DEFINE ('DB_NAME', 'event_app');
 
